dde3c53dab552af85ab72dda0d94cf1a9c37c155
Dylan700 <dylanwilliams700@gmail.com>
2022-02-07 08:54:00 +1100

    Add Calendar Data (WIP)

--- renderer/components/Calendar.js
+++ renderer/components/Calendar.js
@@ -1,11 +1,53 @@
 import { useState, useEffect } from "react";
 import Spacer from "./Spacer";
-import axios from "axios";
 import useTheme from "../contexts/theme";
-import ProgressBar from "./ProgressBar";
+import ICAL from "ical.js";
 
 const Calendar = (props) => {
-	// ical = https://timetable.sydney.edu.au/even/rest/calendar/ical/cea653ea-91a2-4850-a718-297f621ada00
+	const [data, setData] = useState();
+
+	useEffect(() => {
+		// get calendar data then save the parsed ICAL object
+		// need to fix cors stuff with axios https://pratikpc.medium.com/bypassing-cors-with-electron-ab7eaf331605
+		const rawData = `
+BEGIN:VCALENDAR
+PRODID:-//Allocate//iCal4j 1.0//EN
+VERSION:2.0
+CALSCALE:GREGORIAN
+BEGIN:VEVENT
+DTSTAMP:20220205T011823Z
+DTSTART;TZID=Australia/Sydney:20220221T100000
+DTEND;TZID=Australia/Sydney:20220221T120000
+SUMMARY:Introduction to Artificial Intelligence\, LEC
+LOCATION:-
+DESCRIPTION:COMP3308-S1C-ND-RE\, LEC\, 01 Introduction to Artificial Intelligence Staff: - Location: -
+END:VEVENT
+UID:uid0
+END:VCALENDAR
+`;
+		const icalDataParse = ICAL.parse(rawData);
+		const icalData = new ICAL.Component(icalDataParse);
+		setData(icalData);
+	}, []);
+
+	// get the events from the ical where the date matches the current day/year
+	const getEvents = (date, data) => {
+		if (data) {
+			const events = data.getAllSubcomponents("vevent");
+			const filteredEvents = events.filter((e) => {
+				const event = new ICAL.Event(e);
+				return (
+					event.startDate.year === date.getFullYear() &&
+					event.startDate.day === date.getDate() && event.startDate.month-1 === date.getMonth()
+				);
+			});
+			return filteredEvents;
+		}else{
+			return [];
+		}
+	}
+
+
 	
 	// return the date as a percentage between 12am to 12am (24 hours), so 0.5 would be 12pm
 	const dateAsPercentage = (date) => {
@@ -18,7 +60,7 @@ const Calendar = (props) => {
 	}
 
 	// return a div with the given percentage width based on the duration in minutes, and left margin offset based on the time. For example a 30 min event would be 1/48th of the width
-	const getEventDiv = (duration, startTime) => {
+	const getEventDiv = (duration, startTime, summary) => {
 		const percentage = duration / 60 / 24;
 		const left = startTime / 60 / 24;
 		return (
@@ -30,7 +72,7 @@ const Calendar = (props) => {
 					backgroundColor: "var(--primary-color)",
 				}}
 			>
-				<marquee scrollamount={2} className="display text-tiny" style={{color: "var(--background-color)"}}>Event Here</marquee>
+				<marquee scrollamount={2} className="display text-tiny" style={{color: "var(--background-color)"}}>{summary}</marquee>
 			</div>
 		);
 	}
@@ -68,8 +110,11 @@ const Calendar = (props) => {
 					<div className="col" style={{ width: "100%", position: "relative", height: "20px"}}>
 						{/* 2pm event, going for 1 hour */}
 						<span className={`display text-small ${new Date().getDay() == 0 ? "text-primary": "text-secondary" }`}>SUN</span>
+						{getEvents(new Date(2022, 1, 21, 0, 0, 0, 0), data).map((event) => {
+							const e = new ICAL.Event(event);
+							return getEventDiv(e.duration.hours*60, e.startDate.minute + e.startDate.hour * 60, e.summary);
+						})}
 						{getEventDiv(60, 60 * 14)}
-						{getEventDiv(60, 60 * 6)}
 					</div>
 					<div className="col" style={{ width: "100%", position: "relative" , height: "20px"}}>
 						{/* 2pm event, going for 1 hour */}
